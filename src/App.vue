2d48a3ed1ac9762805d6f1840ff46229e480b51f
Valen Gutierrez <valentin.g.l@gmail.com>
2024-07-19 09:24:59 +0200

    Add Typescript to project

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,17 @@
-<script setup>
+<script setup lang="ts">
+import { ref } from 'vue'
 import FileUpload from '@/components/FileUpload.vue'
 
+const data = ref('')
+
 const handleDataChange = (data) => {
-  console.log(data)
+  data.value = data
 }
 </script>
 
 <template>
-  <FileUpload :maxSize="500000" extension="csv" @data="(data) => handleDataChange(data)"/>
+  <FileUpload :maxSize="500000" extension="csv" @data="(data) => handleDataChange(data)" />
+  {{ data }}
 </template>
 
 <style></style>
--- src/components/FileUpload.vue
+++ src/components/FileUpload.vue
@@ -1,4 +1,4 @@
-<script setup>
+<script setup lang="ts">
 import { ref } from 'vue'
 
 const props = defineProps({
@@ -11,14 +11,14 @@ const error = ref(null)
 const file = ref({
   name: '',
   size: 0,
-  extension: '',
+  extension: ''
 })
 
 const handleFileChange = (e) => {
   error.value = null
 
   if (e.target.files && e.target.files[0]) {
-    const upload = e.target.files[0];
+    const upload = e.target.files[0]
 
     file.value.name = upload.name.split('.').shift()
     file.value.extension = upload.name.split('.').pop()
@@ -55,12 +55,12 @@ const isFileExtensionValid = (ext) => {
 </script>
 
 <template>
-  <input type="file" name="" id="" @change="handleFileChange($event)">
-  <div class="info" v-if="!error.value">
+  <input type="file" name="" id="" @change="handleFileChange($event)" />
+  <div class="info" v-if="!error">
     ----
-    <span>File name: {{ file.name || "--" }}</span>
-    <span>File size: {{ file.size || "--" }}</span>
-    <span>File extension: {{ file.extension || "--" }}</span>
+    <span>File name: {{ file.name || '--' }}</span>
+    <span>File size: {{ file.size || '--' }}</span>
+    <span>File extension: {{ file.extension || '--' }}</span>
   </div>
 
   <div v-if="error">
@@ -69,12 +69,12 @@ const isFileExtensionValid = (ext) => {
 </template>
 
 <style>
-  .info {
-     display: flex;
-     flex-wrap: wrap;
-  }
+.info {
+  display: flex;
+  flex-wrap: wrap;
+}
 
-  span {
-    width: 100%;
-  }
+span {
+  width: 100%;
+}
 </style>
